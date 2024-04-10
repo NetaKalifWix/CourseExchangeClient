59a4e1ab05b0ba35a82e710911e4cb627cbbcef4
eran <eransht@wix.com>
2024-04-10 10:18:13 +0300

    fix default option for selectbox

--- src/components/DDCourseList.js
+++ src/components/DDCourseList.js
@@ -7,6 +7,10 @@ const DDCourseList = (props) => {
         value={props.course}
         onChange={(e) => props.setCourse(e.target.value)}
       >
+        <option value="" disabled selected>
+          Select your option
+        </option>
+
         {courses.map((course, index) => (
           <option key={index} value={course}>
             {course}
