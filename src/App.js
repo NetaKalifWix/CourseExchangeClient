ec62e35217b7c66bf58df111694ebeef7de08eb2
eran <eransht@wix.com>
2024-04-10 21:16:48 +0300

    fix prod run

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import Input from "./components/Input";
 import MyExchanges from "./components/MyExchanges";
 import ExchangesList from "./components/ExchangesList";
 
-const url = process.env.REACT_APP_SERVER_URL;
+console.log("process.env.STATUS", process.env.REACT_APP_STATUS);
+console.log("process.env.REACT_APP_LOCAL_SERVER_URL", process.env.REACT_APP_LOCAL_SERVER_URL);
+console.log("process.env.REACT_APP_SERVER_URL", process.env.REACT_APP_SERVER_URL);
+const url = process.env.REACT_APP_STATUS === "prod"
+  ? process.env.REACT_APP_SERVER_URL
+  : process.env.REACT_APP_LOCAL_SERVER_URL;
 // const url = "https://course-exchange-server.onrender.com";
 // const url = "http://localhost:3002";
 
@@ -22,47 +27,46 @@ function App() {
   const [showMyExchanges, setShowMyExchanges] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const validate = (type,val) => {
-    if (type === "name"){
-      if (val === ""){
-        return false
+  const validate = (type, val) => {
+    if (type === "name") {
+      if (val === "") {
+        return false;
       }
     }
     // phone must start with 0 or +972
     // if it starts with +972 it must have 12 digits
     // if it starts with 0 it must have 10 digits
-    const phoneRegex = /^(\+972|0)([0-9]{9,10})$/
-    if (type === "phone"){
-      if (!phoneRegex.test(val)){
-        return false
+    const phoneRegex = /^(\+972|0)([0-9]{9,10})$/;
+    if (type === "phone") {
+      if (!phoneRegex.test(val)) {
+        return false;
       }
     }
-    return true
-  }
+    return true;
+  };
 
   const validateAll = () => {
-    
-    let errorMsg = ""
-    if (currentCourse === ""){
-      errorMsg = "Please select a current course"
+    let errorMsg = "";
+    if (currentCourse === "") {
+      errorMsg = "Please select a current course";
     }
-    if (desiredCourse === ""){
-      errorMsg = "Please select a desired course"
+    if (desiredCourse === "") {
+      errorMsg = "Please select a desired course";
     }
-    if (!validate("name",name)){
-      errorMsg = "Please enter a valid name"
+    if (!validate("name", name)) {
+      errorMsg = "Please enter a valid name";
     }
-    if (!validate("phone",phone)){
-      errorMsg = "Please enter a valid phone number"
+    if (!validate("phone", phone)) {
+      errorMsg = "Please enter a valid phone number";
     }
-    if (errorMsg !== ""){
-      alert(errorMsg)
-      setIsError(true)
-      return false
+    if (errorMsg !== "") {
+      alert(errorMsg);
+      setIsError(true);
+      return false;
     }
 
-    return true
-  }
+    return true;
+  };
 
   useEffect(() => {
     fetch(`${url}`)
@@ -74,8 +78,6 @@ function App() {
       });
   }, []);
 
-
-  
   const updateCycle = () => {
     fetch(`${url}/cycles`)
       .then((response) => response.json())
@@ -86,9 +88,8 @@ function App() {
   };
 
   const handleAddExchange = () => {
-
-    if (!validateAll()){
-      return
+    if (!validateAll()) {
+      return;
     }
     const toSend = {
       exchange: {
@@ -165,8 +166,18 @@ function App() {
           setCourse={setDesiredCourse}
           title="What I Want"
         />
-        <Input set={setname} value={name} label="Your Name (English)" isError ={isError && !validate("name",name)}/>
-        <Input set={setPhone} value={phone} label="Your Phone" isError={isError && !validate("phone",phone)}/>
+        <Input
+          set={setname}
+          value={name}
+          label="Your Name (English)"
+          isError={isError && !validate("name", name)}
+        />
+        <Input
+          set={setPhone}
+          value={phone}
+          label="Your Phone"
+          isError={isError && !validate("phone", phone)}
+        />
         <button onClick={handleAddExchange}>Add Exchange</button>
         <button
           onClick={() =>
